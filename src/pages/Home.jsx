fcf2f5cb9f2f5806d37fcb1b0b0336e83a94f00b
BhushanBagde123 <bhushnabagade7@gmail.com>
2025-02-03 17:59:32 +0530

    add instagram icon

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
     <div className='w-full min-h-screen flex '>
         <div className='w-[70%] h-[900px] flex flex-col  '>
         <StoryNav/>
-        <div className='w-full h-full flex flex-col items-center mt-4 p-3 gap-14'>
+        <div className='w-full h-auto flex flex-col items-center mt-4 p-3 gap-14'>
+        <Content/>
         <Content/>
         <Content/>
         
